ea554e232ac00e7be15dbb27183a3c4c6e55363a
Raiyan Bashir Mahin <u1804097@student.cuet.ac.bd>
2023-04-10 02:52:53 +0600

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1450A | Avoid Trygub/161471016.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main()
+{
+    ll t; cin >> t;
+    while(t--)
+    {
+        ll n; cin >> n; string s; cin >> s;
+        sort(s.begin(), s.end());
+        cout << s << endl;
+        
+        
+    }
+}
