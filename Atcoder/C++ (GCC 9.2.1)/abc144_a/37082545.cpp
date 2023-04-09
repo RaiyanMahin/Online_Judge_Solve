da2a3f9378019c925c779311c2ef3fd16b5a8bba
Raiyan Bashir Mahin <u1804097@student.cuet.ac.bd>
2023-04-10 01:35:23 +0600

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc144_a/37082545.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+        using namespace std;
+
+        typedef long long ll;
+        //#define pb push_back
+
+           int main()
+         {
+             int n,m; cin >> n>>m;
+             if(n<10 && m<10)
+             {
+                 cout << n*m << '\n';
+             }
+             else {
+                cout << -1 << '\n';
+             }
+
+         }
