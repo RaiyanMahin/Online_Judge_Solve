068a28ebf956cdd9a5813bc0e470619cea4c3e0c
Raiyan Bashir Mahin <u1804097@student.cuet.ac.bd>
2023-04-10 01:37:20 +0600

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc184_a/37040102.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main()
+{
+    ll t = 1;
+    while(t--)
+    {
+        ll a, b, c,d; cin >> a >> b >> c >> d;
+        cout << (a * d - b * c) << endl;
+        
+    }
+}
+
+
+
