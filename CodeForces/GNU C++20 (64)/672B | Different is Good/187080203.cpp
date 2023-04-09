2f4bb4e40ccab70207b09046643356643c1c4962
Raiyan Bashir Mahin <u1804097@student.cuet.ac.bd>
2023-04-10 02:14:11 +0600

    Committing files

---
+++ CodeForces/GNU C++20 (64)/672B | Different is Good/187080203.cpp
@@ -0,0 +1,52 @@
+ 
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define endl "\n"
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    ll n; cin >> n; string s; cin >> s;
+    map < char , ll > mp;
+    for(char i : s)
+    {
+       mp[i]++;
+    }
+//    ll mx = -1, su = 0;
+//    for(auto i : mp)
+//    {
+//        mx = max(mx , i.second);
+//        if(i.second > 1)
+//        {
+//            su += (i.second - 1);
+//        }
+//    }
+//    if(mp.size() == n)
+//        return cout << 0, 0;
+//    if(mp.size() == 26 and n > 26)
+//    {
+//        return cout << -1, 0;
+//
+//    }
+//    if(mp.size() == 26 and mx > 1)
+//    {
+//        return cout << -1, 0;
+//
+//    }
+//
+//    if(mp.size() < 26 and mx > 26)
+//        return cout << -1 , 0;
+//        
+//        
+//    if((ll)mp.size() + su > 26)
+//    {
+//        return cout << -1 , 0;
+//        
+//    }
+ 
+     if(n > 26)
+        return cout << -1, 0;
+     return cout << abs(n - (ll)mp.size()), 0;
+ 
+ 
+}
