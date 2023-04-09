7ba1cd7a2fe61a16287c462a8fc85a335bd7fffe
Raiyan Bashir Mahin <u1804097@student.cuet.ac.bd>
2023-04-10 01:36:42 +0600

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc164_a/37080754.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main()
+{
+    ll a, b; cin >> a >> b;
+    if(b >= a)
+        cout << "unsafe\n";
+    else
+        cout << "safe\n";
+}
+
