16171b0948e9ebb9bbb06a044f470f02b064940e
Raiyan Bashir Mahin <u1804097@student.cuet.ac.bd>
2023-04-10 02:20:09 +0600

    Committing files

---
+++ CodeForces/GNU C++20 (64)/451A | Game With Sticks/186706127.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using namespace std;
+int main()
+{
+    int n,m; cin >> n >> m; if(n > m)  swap(n , m );
+    if(n % 2 == 0) cout << "Malvika"; else cout << "Akshat" ; }
