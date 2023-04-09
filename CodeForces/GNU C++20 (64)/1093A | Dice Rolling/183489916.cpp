fed5ccc85e316462c1e5f2e71f470da3e9fc0144
Raiyan Bashir Mahin <u1804097@student.cuet.ac.bd>
2023-04-10 02:31:57 +0600

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1093A | Dice Rolling/183489916.cpp
@@ -0,0 +1,13 @@
+ 
+#include<bits/stdc++.h>
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+typedef long long ll;
+using namespace std;
+int main()
+{
+    
+    int t; cin>>t;  
+    for(int i=1;i<=t;i++){
+        int n; cin>>n; cout<<n/2<<endl; }
+    }
+       
