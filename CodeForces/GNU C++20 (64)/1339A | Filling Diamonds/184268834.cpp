fa3875d89bfe54fc7a41f9180c31f54580904f2f
Raiyan Bashir Mahin <u1804097@student.cuet.ac.bd>
2023-04-10 02:27:16 +0600

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1339A | Filling Diamonds/184268834.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using namespace std;
+int main()
+{
+    ll t; cin >> t;
+    while(t--)
+    {
+        ll n; cin >> n;
+        cout << n << '\n';
+    }
+}
