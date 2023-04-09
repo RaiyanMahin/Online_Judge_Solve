57b9ba2f04b0be8354929d0eaf47f4337c39dc46
Raiyan Bashir Mahin <u1804097@student.cuet.ac.bd>
2023-04-10 02:28:06 +0600

    Committing files

---
+++ CodeForces/GNU C++14/1372A | Omkar and Completion/184265133.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using namespace std;
+#define debug(x) cout << " --- >" << x << '\n'
+int main()
+{
+    ll t; cin >> t;
+    while(t--)
+    {
+        ll n; cin >> n;
+        vector < ll > b(n , 1);
+        for(auto i : b)
+        {
+            cout << i << ' ';
+            
+        }
+        cout << '\n';
+        b.clear();
+ 
+ 
+    }
+}
