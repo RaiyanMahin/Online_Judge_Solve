34e221b11493877618dd4fbf43f25e485cfc05b3
Raiyan Bashir Mahin <u1804097@student.cuet.ac.bd>
2023-04-10 02:28:21 +0600

    Committing files

---
+++ CodeForces/GNU C++14/1353A | Most Unstable Array/184264531.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll =  long long;
+int main()
+{
+    ll t; cin >> t;
+    while(t--)
+    {
+        ll n, k; cin >> n >> k;
+        if(n == 1)
+            cout << 0 << '\n';
+        if(n == 2)
+            cout << k << '\n';
+        if(n > 2)
+        {
+            cout << 2 * k << '\n';
+        }
+           
+ 
+ 
+ 
+ 
+        }
+}
+ 
