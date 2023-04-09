5fb09893069f4d92015d7de6c5e2db6c94311c76
Raiyan Bashir Mahin <u1804097@student.cuet.ac.bd>
2023-04-10 02:32:20 +0600

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1337A | Ichihime and Triangle/183488144.cpp
@@ -0,0 +1,19 @@
+///loser
+ 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+using ll = long long int ;
+int main()
+{
+    ll tcc; cin >> tcc;
+    while(tcc--)
+    {
+        ll a,b,c,d; cin >> a >> b >> c >> d;
+       
+ 
+ 
+        cout << b << ' ' << c << ' ' << c << '\n';
+ 
+    }
+}
