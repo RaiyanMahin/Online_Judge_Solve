df9df05202acb350ec8b7edca61c0aeaacce3b94
Raiyan Bashir Mahin <u1804097@student.cuet.ac.bd>
2023-04-10 01:59:17 +0600

    Committing files

---
+++ CodeForces/GNU C++20 (64)/670D2 | Magic Powder - 2/191187322.cpp
@@ -0,0 +1,78 @@
+//long long unsigned
+#include <bits/stdc++.h>
+using namespace std;
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx,avx2,fma")
+//#pragma GCC target ("avx2")
+//#pragma GCC optimization ("O3")
+//#pragma GCC optimization ("unroll-loops")
+ 
+#define FIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+ 
+using ll = long long ;
+ 
+#define sz(x) (ll)x.size()
+#define nn endl
+ 
+ 
+ 
+ 
+const ll OP = 100009;
+ll n, kk;
+vector < ll > a(OP, 0), b(OP, 0);
+ 
+ll F(ll mid)
+{
+    ll o = 0;
+    for(ll i = 0; i < n; i++)
+    {
+        ll k = a[i] * mid;
+ 
+        if(k > b[i])
+        {
+            o += (k - b[i]);
+            if(o >= 1e10)
+            {
+                return o;
+            }
+        }
+    }
+    return o;
+}
+ 
+void baal()
+{
+    cin >> n >> kk;
+    
+    a.resize(n); b.resize(n); 
+ 
+    for(ll &i : a) cin >> i; for(ll &i : b) cin >> i;
+ 
+    ll low = 0, high = 2e9, ans = 0;
+ 
+    while (low <= high)
+    {
+        ll mid = (low + high) >> 1;
+        if(F(mid) > kk)
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            ans = mid;
+            low = mid + 1;
+        }
+    }
+    cout << ans << nn; return;
+    
+ 
+   
+}
+ 
+int main()
+{
+     ios_base::sync_with_stdio(false); cin.tie(0);
+ 
+        baal();
+}
+//https://codeforces.com/contest/670/submission/80804698
