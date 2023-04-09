d5f231d08b4eea99cf1541af8bd42f470a330a07
Raiyan Bashir Mahin <u1804097@student.cuet.ac.bd>
2023-04-10 02:04:56 +0600

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1445B | Elimination/190221062.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    ll t; cin >> t;
+    while(t--)
+    {
+        ll a ,b,c,d; cin >> a >> b >> c >> d;
+        cout << max(a + b , c + d) << endl;
+ 
+ 
+ 
+    }
+ 
+ 
+}
