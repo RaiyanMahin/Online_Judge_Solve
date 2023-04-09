03b97b4a0ed4e7ede522d72d8e1cbf83430584c9
Raiyan Bashir Mahin <u1804097@student.cuet.ac.bd>
2023-04-10 01:36:28 +0600

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc122_a/37080847.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using namespace std;
+int main()
+{
+    char a; cin >> a;
+    if(a == 'A')
+        cout << "T\n";
+    if(a == 'T')
+        cout << "A\n";
+    if(a == 'C')
+        cout << "G\n";
+    if(a == 'G')
+        cout << "C\n";
+    //double x; cin >> x; cout <<  fixed << setprecision(10) << ( (x * x * x) / 27.0 ) << '\n';
+}
