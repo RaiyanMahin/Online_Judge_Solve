d21f4ff9595fcf266ea0e91c44e679640f83ab5d
Raiyan Bashir Mahin <u1804097@student.cuet.ac.bd>
2023-04-10 02:05:11 +0600

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1438A | Specific Tastes of Andre /190220735.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define endl "\n"
+map < ll ,ll > mp;
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    ll t; cin >> t;
+    while(t--)
+    {
+        ll n; cin >> n;
+        while(n--)
+        {
+            cout << "1 ";
+        }
+        cout << endl;
+ 
+    }
+ 
+ 
+}
