daec3457b7a61bc8f189510111327b86e6492b67
Raiyan Bashir Mahin <u1804097@student.cuet.ac.bd>
2023-04-10 02:32:52 +0600

    Committing files

---
+++ CodeForces/GNU C++20 (64)/200B | Drinks/183487117.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,i,s=0; double q; cin>>n; int a[n];
+    for(int i=0;i<n;i++)
+    {  cin>>a[i]; 
+    s+=a[i]; 
+        
+    } 
+    q=(double)s/n; cout<<q<<endl; 
+    }
