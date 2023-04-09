067e5eee8cdef122ace8d26eb21552bf8baad3c7
Raiyan Bashir Mahin <u1804097@student.cuet.ac.bd>
2023-04-10 01:36:39 +0600

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc163_a/37080766.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h> 
+using namespace std;
+#define PI acos(-1)
+using ld = long double;
+int main()
+{
+    ld r; cin >> r;
+        ld R = 2 * PI * r;
+        cout << fixed << setprecision(20) << R << '\n';
+
+}
+
+
