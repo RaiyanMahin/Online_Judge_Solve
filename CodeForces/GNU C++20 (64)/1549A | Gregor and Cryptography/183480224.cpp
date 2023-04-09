d1e907c87a79d9edb72d0a1f3d966781f494361a
Raiyan Bashir Mahin <u1804097@student.cuet.ac.bd>
2023-04-10 02:35:16 +0600

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1549A | Gregor and Cryptography/183480224.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define debug(x) cerr << # x << ' ' << x << endl;
+ 
+void baal()
+{
+ 
+    ll n; cin >> n;
+    cout << (n / 2) << ' ' << (n - 1) << endl;
+    return;
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    return;
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+}
+ 
+int main()
+{
+    ll t; cin >> t;
+    while(t--)
+        baal();
+}
