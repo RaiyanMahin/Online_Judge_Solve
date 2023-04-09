077fadff69d632819dd279ae4eccc76664543e38
Raiyan Bashir Mahin <u1804097@student.cuet.ac.bd>
2023-04-10 01:36:10 +0600

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc154_b/37081209.cpp
@@ -0,0 +1,13 @@
+///RAIYAN_MAHIN
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main()
+{
+    string s; cin >> s ; ll x = s.size();
+    char X = 'x';
+    for(ll i = 0; i < x; i++)
+    {
+        cout << X;
+    }
+}
