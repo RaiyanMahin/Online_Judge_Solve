42bc091d56df558f13fcf0ebbdd0425ce374ee04
Raiyan Bashir Mahin <u1804097@student.cuet.ac.bd>
2023-04-10 01:36:32 +0600

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc161_a/37080818.cpp
@@ -0,0 +1,30 @@
+//s2.clear();
+//        s2 = s.substr(i, d);
+//        for(j = 0; j < s2.size() ; j++)
+//            {
+//                if(s2[j] != s1[j])
+//                {
+//                    boo++;
+//                }
+//            }
+//            if(boo == s1.size())
+#include <bits/stdc++.h>
+using ll = long long;
+using namespace std;
+int main()
+{
+    ll x,y,z; cin >> x >> y >> z;
+    swap(x,y);
+    swap(x,z);
+   cout << x << ' ' << y << ' '<< z << '\n';
+
+
+
+
+
+
+
+
+
+
+}
