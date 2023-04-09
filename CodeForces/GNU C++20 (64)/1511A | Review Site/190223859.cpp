2fb3af147d64d6d7082311163e75ffa7fa56d808
Raiyan Bashir Mahin <u1804097@student.cuet.ac.bd>
2023-04-10 02:03:55 +0600

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1511A | Review Site/190223859.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    ll t; cin >> t;
+    while(t--)
+    {
+        ll c = 0;
+        ll n; cin >> n;
+        while(n--)
+        {
+            ll x; cin >> x;
+            if(x == 1 or x == 3)
+                c++;
+        }
+        cout << c << endl;
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    }
+}
+ 
+//int main()
+//{
+//    ll t; cin >> t;
+//    while(t--)
+//    {
+//        string s; cin >> s; ll x = s.size(); ll n; cin >> n; string w(x, '0');
+//        ll baal = 0;
+//        for(ll i = 0; i < x; i++)
+//        {
+//            if( i + n < x )
+//            {
+//                if(s[i + n] == '1')
+//                    w[i + n] = '1';
+//            }
+//            else if(i )
+//        }
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//    }
+//}
