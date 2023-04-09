296cedc102c00cca2edede2816b09eef4e86bc3b
Raiyan Bashir Mahin <u1804097@student.cuet.ac.bd>
2023-04-10 01:37:57 +0600

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc193_a/37038950.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long double;
+#define endl "\n"
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+    ll a , b; cin >> a >> b;
+    ll o = a - b;
+    ll p = (o / a) * 100;
+    cout << fixed << setprecision(18) << p << endl;
+
+
+
+
+
+
+
+}
