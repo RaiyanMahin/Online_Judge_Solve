92ed3602ef68bab9df7789ee5b9f3feaa77409b8
Raiyan Bashir Mahin <u1804097@student.cuet.ac.bd>
2023-04-10 02:11:55 +0600

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1455C | Ping-pong/187842320.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    ll t; cin >> t;
+    while(t--)
+    {
+        ll x , y; cin >> x >> y;
+        cout << x - 1 << ' ' << y << endl;
+        
+        
+        
+        
+    }
+}
+ 
