15ae36e7e9feadee0b50204ca34927155866dc34
Raiyan Bashir Mahin <u1804097@student.cuet.ac.bd>
2023-04-10 01:38:13 +0600

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc214_a/37038777.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define debug(x) cerr << # x << ' ' << x << endl;
+
+void baal()
+{
+    ll n; cin >> n;
+    if(n <= 125)
+    {
+        cout << "4\n"; return;
+    }
+    else if(n > 125 and n < 212)
+    {
+        cout << "6\n"; return;
+
+    }
+    else
+    {
+        cout << "8\n"; return;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return;
+
+}
+
+
+int main()
+{
+    ll t = 1; //cin >> t;
+    while(t--)
+        baal();
+}
