35e4109a1e7d63370353b9b25aafe13f62476a8a
Raiyan Bashir Mahin <u1804097@student.cuet.ac.bd>
2023-04-10 01:36:26 +0600

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc159_c/37080883.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using namespace std;
+int main()
+{
+    double x; cin >> x; cout <<  fixed << setprecision(10) << ( (x * x * x) / 27.0 ) << '\n';
+}
