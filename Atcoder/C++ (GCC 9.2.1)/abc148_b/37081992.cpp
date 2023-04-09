720ce36c8775557d29204279797e970b598a3dc2
Raiyan Bashir Mahin <u1804097@student.cuet.ac.bd>
2023-04-10 01:35:38 +0600

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc148_b/37081992.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+#define md 1000000007
+int main()
+{
+    int m; cin >> m;
+     char a[100],b[100],c[100]; cin >> a >> b; ll d = strlen(a);
+    for(ll i =0; i < d ;i++)
+
+    {
+        //c[i] = a[i] + b[i];
+        printf("%c%c",a[i],b[i]);
+    }
+
+
+}
