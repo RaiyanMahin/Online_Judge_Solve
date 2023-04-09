ace4f1e65e15dd68f0ac328123da00afc834f68f
Raiyan Bashir Mahin <u1804097@student.cuet.ac.bd>
2023-04-10 02:10:59 +0600

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1433E | Two Round Dances/188147173.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = unsigned long long int;
+int main()
+{
+    ll n; cin >> n;
+    if(n == 2)
+    {
+         cout << 1 << endl;
+        
+    }
+     if(n == 4)
+    {
+         cout << 3 << endl;
+        
+    }
+     if(n == 8)
+    {
+         cout << 1260 << endl;
+        
+    }
+     if(n == 6)
+    {
+        cout << 40 << endl;
+        
+    }
+     if(n == 10)
+    {
+        cout << 72576 << endl;
+        
+    }
+     if(n == 12)
+    {
+        cout << 6652800 << endl;
+        
+    }
+     if(n == 14)
+    {
+        cout << 889574400 << endl;
+        
+        
+    }
+     if(n == 16)
+    { 
+        cout << 163459296000 << endl;
+        
+    }
+     if(n == 18)
+    {
+        cout << 39520825344000 << endl;
+        
+        
+    }
+     if(n == 20)
+    {
+        cout << 12164510040883200 << endl;
+ 
+        
+    }
+ 
+ 
+ 
+ 
+}
+ 
+ 
+ 
+ 
+ 
