56535f5098faece649fa370befc9f15a49f96d1f
Raiyan Bashir Mahin <u1804097@student.cuet.ac.bd>
2023-04-10 02:05:14 +0600

    Committing files

---
+++ CodeForces/GNU C++20 (64)/1437A | Marketing Scheme/190220620.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define ll long long
+ 
+using namespace std;
+ 
+ 
+void baal()
+{
+    ll a,  b; cin >> a >> b;
+    if(a * 2 > b)
+        cout << "YES\n";
+    if(a * 2 <= b)
+        cout << "NO\n";
+ 
+    return;
+ 
+ 
+}
+ 
+int main()
+{
+ 
+    ll t; cin >> t;
+    while(t--)
+    {
+        baal();
+ 
+ 
+    }
+ 
+}
