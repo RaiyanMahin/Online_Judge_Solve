f68e605ad5870952500f3136c0af81fa69a740ab
Raiyan Bashir Mahin <u1804097@student.cuet.ac.bd>
2023-04-10 01:37:14 +0600

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/arc104_a/37040182.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    ll a , b; cin >> a >> b; ll c = a + b; ll d = a - b; cout << c / 2 << ' ' << d / 2;
+}
