e43dde178151cd2015a3dc77ee59d489b2f77498
Raiyan Bashir Mahin <u1804097@student.cuet.ac.bd>
2023-04-10 02:28:43 +0600

    Committing files

---
+++ CodeForces/GNU C++14/1269A | Equation/184263666.cpp
@@ -0,0 +1,9 @@
+#include "bits/stdc++.h"
+using ll = long long int;
+using namespace std;
+int main()
+{
+    ll m; cin >> m;
+    if(m == 1) cout << 9 << ' ' << 8 << '\n';
+    else cout << (9 * m) << ' ' <<  (8*m) << '\n';
+}
