b72e27f7a4417241e40a7f0a37d8224d21a7c108
Raiyan Bashir Mahin <u1804097@student.cuet.ac.bd>
2023-04-10 01:36:02 +0600

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc129_a/37081390.cpp
@@ -0,0 +1,14 @@
+/// #RM
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main()
+{
+  vector<int >r;
+  //set<int>::iterator it;
+  int a,b,c; cin >> a >> b >> c; r.push_back(a); r.push_back(b); r.push_back(c); sort(r.begin(),r.end());
+  cout << r[0] + r[1] << '\n';
+
+
+
+}
